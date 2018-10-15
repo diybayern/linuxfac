17dc6aa4c69ace397c639466e02725dfa69540a3
yanghuiting <yanghuiting@ruijie.com.cn>
2018-10-15 15:04:28 +0800

    fix mem_test.sh

--- factory_test/scripts/mem_test.sh
+++ factory_test/scripts/mem_test.sh
@@ -26,99 +26,99 @@ resultWalkingZeroes=`cat $mem_result_file | grep "ok" | awk '/Walking Zeroes/ {p
 result8bitWrites=`cat $mem_result_file | grep "ok" | awk '/8-bit Writes/ {print $1}'`
 result16bitWrites=`cat $mem_result_file | grep "ok" | awk '/16-bit Writes/ {print $1}'`
 
-if [ -n $resultStuckAddress ]; then
+if [ -n "$resultStuckAddress" ]; then
     echo "Stuck Address:\t\tok" >> $mem_ui_log
 else
     echo "Stuck Address:\t\tfail" >> $mem_ui_log
 fi
 
-if [ -n $resultRandomValue ]; then
+if [ -n "$resultRandomValue" ]; then
     echo "Random Value:\t\tok" >> $mem_ui_log
 else 
     echo "Random Value:\t\tfail" >> $mem_ui_log
 fi
 
-if [ -n $resultCompareXOR ]; then
+if [ -n "$resultCompareXOR" ]; then
     echo "Compare XOR:\t\tok" >> $mem_ui_log
 else 
     echo "Compare XOR:\t\tfail" >> $mem_ui_log
 fi
 
-if [ -n $resultCompareSUB ]; then 
+if [ -n "$resultCompareSUB" ]; then 
     echo "Compare SUB:\t\tok" >> $mem_ui_log 
 else 
     echo "Compare SUB:\t\tfail" >> $mem_ui_log 
 fi
 
-if [ -n $resultCompareMUL ]; then 
+if [ -n "$resultCompareMUL" ]; then 
     echo "Compare MUL:\t\tok" >> $mem_ui_log 
 else 
     echo "Compare MUL:\t\tfail" >> $mem_ui_log 
 fi
-if [ -n $resultCompareDIV ]; then 
+if [ -n "$resultCompareDIV" ]; then 
     echo "Compare DIV:\t\tok" >> $mem_ui_log 
 else 
     echo "Compare DIV:\t\tfail" >> $mem_ui_log 
 fi
-if [ -n $resultCompareOR ]; then 
+if [ -n "$resultCompareOR" ]; then 
     echo "Compare OR:\t\tok" >> $mem_ui_log 
 else 
     echo "Compare OR:\t\tfail" >> $mem_ui_log 
 fi
-if [ -n $resultCompareAND ]; then 
+if [ -n "$resultCompareAND" ]; then 
     echo "Compare AND:\t\tok" >> $mem_ui_log 
 else 
     echo "Compare AND:\t\tfail" >> $mem_ui_log 
 fi
-if [ -n $resultSequentialIncrement ]; then 
+if [ -n "$resultSequentialIncrement" ]; then 
     echo "Sequential Increment:\t\tok" >> $mem_ui_log 
 else
     echo "Sequential Increment:\t\tfail" >> $mem_ui_log 
 fi
-if [ -n $resultSolidBits ]; then 
+if [ -n "$resultSolidBits" ]; then 
     echo "Solid Bits:\t\t\tok" >> $mem_ui_log 
 else
     echo "Solid Bits:\t\t\tfail" >> $mem_ui_log 
 fi
-if [ -n $resultBlockSequential ]; then 
+if [ -n "$resultBlockSequential" ]; then 
     echo "Block Sequential:\t\tok" >> $mem_ui_log 
 else
     echo "Block Sequential:\t\tfail" >> $mem_ui_log 
 fi
-if [ -n $resultCheckerboard ]; then 
+if [ -n "$resultCheckerboard" ]; then 
     echo "Checkerboard:\t\tok" >> $mem_ui_log 
 else
     echo "Checkerboard:\t\tfail" >> $mem_ui_log 
 fi
-if [ -n $resultBitSpread ]; then 
+if [ -n "$resultBitSpread" ]; then 
     echo "Bit Spread:\t\t\tok" >> $mem_ui_log 
 else
     echo "Bit Spread:\t\t\tfail" >> $mem_ui_log 
 fi
 
-if [ -n $resultBitFlip ]; then 
+if [ -n "$resultBitFlip" ]; then 
     echo "Bit Flip:\t\t\tok" >> $mem_ui_log 
 else
     echo "Bit Flip:\t\t\tfail" >> $mem_ui_log 
 fi
-if [ -n $resultWalkingOnes ]; then 
+if [ -n "$resultWalkingOnes" ]; then 
     echo "Walking Ones:\t\tok" >> $mem_ui_log 
 else
     echo "Walking Ones:\t\tfail" >> $mem_ui_log 
 fi
-if [ -n $resultWalkingZeroes ]; then 
+if [ -n "$resultWalkingZeroes" ]; then 
     echo "Walking Zeroes:\t\tok" >> $mem_ui_log 
 else
     echo "Walking Zeroes:\t\tfail" >> $mem_ui_log 
 fi
 
-if [ -n $result8bitWrites ]; then 
+if [ -n "$result8bitWrites" ]; then 
     echo "8-bit Writes:\t\t\tok" >> $mem_ui_log 
 else
     echo "8-bit Writes:\t\t\tfail" >> $mem_ui_log 
 fi
 
-if [ -n $result16bitWrites ]; then 
+if [ -n "$result16bitWrites" ]; then 
     echo "16-bit Writes:\t\tok" >> $mem_ui_log 
 else
     echo "16-bit Writes:\t\tfail" >> $mem_ui_log 
